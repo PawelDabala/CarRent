992f099f34c65726da4d500b8346ed2cba0f0827
Paweł Dąbała <Pawel.Dabala@mediadirection.com.pl>
2020-02-07 10:30:05 +0100

    Fixed: err with urlpattern

--- CarRentPro/CarRentPro/urls.py
+++ CarRentPro/CarRentPro/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('CarRentApp.ulrs'))
+    path('', include('CarRentApp.urls'))
 ]
