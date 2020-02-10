2b833e284c9b29810e8cc5a2cf5b914c9fee568b
Paweł Dąbała <Pawel.Dabala@mediadirection.com.pl>
2020-02-10 10:38:00 +0100

    add view for User

--- CarRentPro/CarRentApp/urls.py
+++ CarRentPro/CarRentApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
+from .views import User_view
 # from .views import
 
 urlpatterns = [
-    
-
+    path('users/', User_view, name='users'),
 ]
--- CarRentPro/CarRentApp/views.py
+++ CarRentPro/CarRentApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import User, Address
 
 # Create your views here.
+def User_view(request):
+    users = User.objects.all()
+    context = {
+        'users': users
+    }
+
+    return render(request, 'users.html', context=context)
