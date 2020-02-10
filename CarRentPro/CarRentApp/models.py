5c7aca396e30bca7a7962aa6232a448e9c7752f0
Paweł Dąbała <Pawel.Dabala@mediadirection.com.pl>
2020-02-10 10:15:45 +0100

    add: models User and Address

--- CarRentPro/CarRentApp/models.py
+++ CarRentPro/CarRentApp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    """Model definition for User."""
+
+    # TODO: Define fields here
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):
+    """Model definition for Address."""
+
+    # TODO: Define fields here
+    street = models.CharField(max_length=50)
+    bulding = models.CharField(max_length=50)
+    flat = models.CharField(max_length=50)
+    post_code = models.DecimalField(max_digits=5, decimal_places=0)
+    city = models.CharField(max_length=50)
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+        """Unicode representation of Address."""
+        return self.street
+
+    
