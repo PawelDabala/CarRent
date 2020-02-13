ac11851a6d388868316ae2217d47a1c480e4c6c4
Paweł Dąbała <Pawel.Dabala@mediadirection.com.pl>
2020-02-13 10:13:59 +0100

    add: user_add tablate and views

--- CarRentPro/CarRentApp/urls.py
+++ CarRentPro/CarRentApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import User_view, Address_view
+from .views import User_view, Address_view, User_add
 # from .views import
 
 urlpatterns = [
     path('users/', User_view, name='users'),
     path('addresses/', Address_view, name='addresses'),
+    path('users/add', User_add, name='user_add'),
 ]
--- CarRentPro/CarRentApp/views.py
+++ CarRentPro/CarRentApp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import User, Address
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+
+
 def User_view(request):
     users = User.objects.all()
     context = {
@@ -9,6 +13,18 @@ def User_view(request):
     }
 
     return render(request, 'users.html', context=context)
+
+
+def User_add(request):
+    if request.method == "GET":
+        return render(request, 'user_add.html')
+    elif request.method == 'POST':
+        name = request.POST.get('name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('password')
+        User.objects.create(name=name, last_name=last_name, password=password)
+        return HttpResponseRedirect(reverse('users'))
+
 
 def Address_view(request):
     addresses = Address.objects.all()
